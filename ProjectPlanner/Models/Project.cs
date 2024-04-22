56452b46639005ad4c2212b8c115b7e591210e6a
joaquim6399 <joaquim6399@gmail.com>
2024-04-22 17:49:31 +0100

    Add Project.cs class model

---
+++ ProjectPlanner/Models/Project.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ProjectPlanner.Models
+{
+    public class Project
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+    }
+}
