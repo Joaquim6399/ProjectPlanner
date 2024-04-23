3d4c3efd4d8be986c04e710e69ab88e86e596914
joaquim6399 <joaquim6399@gmail.com>
2024-04-23 15:04:08 +0100

    Add Ticket model to project

---
+++ ProjectPlanner/Models/Ticket.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ProjectPlanner.Models
+{
+    public class Ticket
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Status { get; set; }
+        public string Priority { get; set; }
+        public DateTime CreatedOn { get; set; }
+        public int ProjectId { get; set; }
+        [ForeignKey("ProjectId")]
+        public Project Project { get; set; }
+
+    }
+}
