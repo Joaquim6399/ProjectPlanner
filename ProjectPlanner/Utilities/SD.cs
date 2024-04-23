506c126338d1cafc1b955e9204f6a712478c6ea0
joaquim6399 <joaquim6399@gmail.com>
2024-04-23 15:12:51 +0100

    Add SD class with status and priority values

---
+++ ProjectPlanner/Utilities/SD.cs
@@ -0,0 +1,14 @@
+﻿namespace ProjectPlanner.Utilities
+{
+    public static class SD
+    {
+        public static readonly string status_new = "New";
+        public static readonly string status_waiting = "Waiting for more info";
+        public static readonly string status_progress = "In progress";
+        public static readonly string status_done = "Done";
+
+        public static readonly string priority_high = "High";
+        public static readonly string priority_medium = "Medium";
+        public static readonly string priority_low = "Low";
+    }
+}
