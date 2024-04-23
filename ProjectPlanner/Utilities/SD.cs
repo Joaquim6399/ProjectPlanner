cb68fe21d5c8c0f33ccd9d6f625ccba312d48b9b
joaquim6399 <joaquim6399@gmail.com>
2024-04-23 16:18:33 +0100

    Add SD class to project
    To avoid using magic string create a static class to get these values

--- ProjectPlanner/Utilities/SD.cs
+++ ProjectPlanner/Utilities/SD.cs
@@ -6,9 +6,12 @@
         public static readonly string status_waiting = "Waiting for more info";
         public static readonly string status_progress = "In progress";
         public static readonly string status_done = "Done";
+        
 
         public static readonly string priority_high = "High";
         public static readonly string priority_medium = "Medium";
         public static readonly string priority_low = "Low";
+
+        
     }
 }
