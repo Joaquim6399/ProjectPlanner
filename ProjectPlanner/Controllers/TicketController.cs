95a309f17e1632900d41864ea0ea37925089c687
joaquim6399 <joaquim6399@gmail.com>
2024-04-23 15:42:08 +0100

    Add logic to Create action method

--- ProjectPlanner/Controllers/TicketController.cs
+++ ProjectPlanner/Controllers/TicketController.cs
@@ -1,12 +1,29 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using ProjectPlanner.Data;
+using ProjectPlanner.Models.ViewModels;
 
 namespace ProjectPlanner.Controllers
 {
     public class TicketController : Controller
     {
+        private readonly ApplicationDbContext _db;
+        public TicketController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
         public IActionResult Index()
         {
             return View();
         }
+
+        public IActionResult Create(int projectId)
+        {
+            TicketBoardVM ticketBoard = new TicketBoardVM();
+            ticketBoard.Project = _db.Projects.FirstOrDefault(u => u.Id == projectId);
+
+
+            return View(ticketBoard);
+        }
+
     }
 }
