b196b10dc8bdac2a6c90497a16ef3c0b5887a820
joaquim6399 <joaquim6399@gmail.com>
2024-04-23 15:21:48 +0100

    Seed data to Ticket table in database

--- ProjectPlanner/Data/ApplicationDbContext.cs
+++ ProjectPlanner/Data/ApplicationDbContext.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using ProjectPlanner.Models;
+using ProjectPlanner.Utilities;
 
 namespace ProjectPlanner.Data
 {
@@ -18,6 +19,14 @@ namespace ProjectPlanner.Data
             modelBuilder.Entity<Project>().HasData(
                 new Project { Id = 1, Name = "Mahatan Project", Description = "Create biggest bomb ever" },
                 new Project { Id = 2, Name = "Apollo Project", Description = "Put man on the moon" }
+            );
+
+            modelBuilder.Entity<Ticket>().HasData(
+                new Ticket { Id=1 , Title= "Split Atom", CreatedOn = DateTime.Now, Description = "Succesfully split a atom", Priority = SD.priority_medium, ProjectId = 1, Status = SD.status_new},
+                new Ticket { Id=2 , Title= "Find Plutonium", CreatedOn = DateTime.Now, Description = "Find plutunium for the bomb", Priority = SD.priority_high, ProjectId = 1, Status = SD.status_new},
+                new Ticket { Id=3 , Title= "Find Astronaut", CreatedOn = DateTime.Now, Description = "Find someone willingly to go to the moon", Priority = SD.priority_low, ProjectId = 2, Status = SD.status_new},
+
+                new Ticket { Id=4 , Title= "Create w16 engine", CreatedOn = DateTime.Now, Description = "Create engine with w16 orientation", Priority = SD.priority_medium, ProjectId = 4, Status = SD.status_new}
             );
 
         }
