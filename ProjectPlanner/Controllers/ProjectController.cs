038deb67fbfd022d48857c5c500d5dd88b286a34
joaquim6399 <joaquim6399@gmail.com>
2024-04-23 11:31:33 +0100

    Add applicationDbContext to project controller
    To allow us to use entity framework dbContext methods to access and manage database data and its tables

--- ProjectPlanner/Controllers/ProjectController.cs
+++ ProjectPlanner/Controllers/ProjectController.cs
@@ -1,9 +1,16 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using ProjectPlanner.Data;
 
 namespace ProjectPlanner.Controllers
 {
     public class ProjectController : Controller
     {
+        private readonly ApplicationDbContext _db;
+
+        public ProjectController(ApplicationDbContext db)
+        {
+            _db = db; 
+        }
         public IActionResult Index()
         {
             return View();
