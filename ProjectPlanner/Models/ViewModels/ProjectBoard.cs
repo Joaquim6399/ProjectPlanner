2df1b87b44c9a70737b3424b2d062bdabc518df7
joaquim6399 <joaquim6399@gmail.com>
2024-04-23 15:05:35 +0100

    Add ViewModel for project board
    Where we will use this view model to display the project and its tickets to the user

---
+++ ProjectPlanner/Models/ViewModels/ProjectBoard.cs
@@ -0,0 +1,9 @@
+﻿namespace ProjectPlanner.Models.ViewModels
+{
+    public class ProjectBoard
+    {
+        public Project Project { get; set; }
+        public IEnumerable<Ticket> ListOfTickets { get; set; }
+
+    }
+}
