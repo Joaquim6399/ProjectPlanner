2f9c0b699c919447f9bb4b586d93797b5e0165a8
joaquim6399 <joaquim6399@gmail.com>
2024-04-23 15:35:53 +0100

    Add ticket controller to project

---
+++ ProjectPlanner/Controllers/TicketController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ProjectPlanner.Controllers
+{
+    public class TicketController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
