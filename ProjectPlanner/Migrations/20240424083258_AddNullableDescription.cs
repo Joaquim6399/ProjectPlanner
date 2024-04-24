14c345b9405763b29e9644605f1adc4d961ded45
joaquim6399 <joaquim6399@gmail.com>
2024-04-24 09:37:02 +0100

    Add nullable to description
    description can be an empty field, before it was required to POST

---
+++ ProjectPlanner/Migrations/20240424083258_AddNullableDescription.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ProjectPlanner.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddNullableDescription : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Projects",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.UpdateData(
+                table: "Tickets",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "CreatedOn",
+                value: new DateTime(2024, 4, 24, 9, 32, 57, 861, DateTimeKind.Local).AddTicks(7682));
+
+            migrationBuilder.UpdateData(
+                table: "Tickets",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "CreatedOn",
+                value: new DateTime(2024, 4, 24, 9, 32, 57, 861, DateTimeKind.Local).AddTicks(7730));
+
+            migrationBuilder.UpdateData(
+                table: "Tickets",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "CreatedOn",
+                value: new DateTime(2024, 4, 24, 9, 32, 57, 861, DateTimeKind.Local).AddTicks(7733));
+
+            migrationBuilder.UpdateData(
+                table: "Tickets",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "CreatedOn",
+                value: new DateTime(2024, 4, 24, 9, 32, 57, 861, DateTimeKind.Local).AddTicks(7736));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Projects",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.UpdateData(
+                table: "Tickets",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "CreatedOn",
+                value: new DateTime(2024, 4, 24, 9, 26, 42, 79, DateTimeKind.Local).AddTicks(9626));
+
+            migrationBuilder.UpdateData(
+                table: "Tickets",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "CreatedOn",
+                value: new DateTime(2024, 4, 24, 9, 26, 42, 79, DateTimeKind.Local).AddTicks(9676));
+
+            migrationBuilder.UpdateData(
+                table: "Tickets",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "CreatedOn",
+                value: new DateTime(2024, 4, 24, 9, 26, 42, 79, DateTimeKind.Local).AddTicks(9679));
+
+            migrationBuilder.UpdateData(
+                table: "Tickets",
+                keyColumn: "Id",
+                keyValue: 4,
+                column: "CreatedOn",
+                value: new DateTime(2024, 4, 24, 9, 26, 42, 79, DateTimeKind.Local).AddTicks(9682));
+        }
+    }
+}
--- ProjectPlanner/Models/Project.cs
+++ ProjectPlanner/Models/Project.cs
@@ -8,7 +8,7 @@ namespace ProjectPlanner.Models
         [Required]
         [StringLength(60,MinimumLength =3)]
         public string Name { get; set; }
-        public string Description { get; set; }
+        public string? Description { get; set; }
 
     }
 }
