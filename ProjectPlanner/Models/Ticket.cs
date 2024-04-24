a51caf6ad1ecfd88312dc0c86a99bb72d483c3b7
joaquim6399 <joaquim6399@gmail.com>
2024-04-24 09:58:50 +0100

    Add data annotation for validation

--- ProjectPlanner/Models/Ticket.cs
+++ ProjectPlanner/Models/Ticket.cs
@@ -1,11 +1,16 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace ProjectPlanner.Models
 {
     public class Ticket
     {
         public int Id { get; set; }
+        [Required]
+        [StringLength(60,MinimumLength =3)]
         public string Title { get; set; }
+        [Required]
+        [StringLength(60,MinimumLength =5)]
         public string Description { get; set; }
         public string Status { get; set; }
         public string Priority { get; set; }
