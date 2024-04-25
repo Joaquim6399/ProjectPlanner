9c7c98f32c4d371e6172227f9769a3ce085f7ffd
joaquim6399 <joaquim6399@gmail.com>
2024-04-25 10:03:59 +0100

    Add identity user to project model

--- ProjectPlanner/Models/Project.cs
+++ ProjectPlanner/Models/Project.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
 
 namespace ProjectPlanner.Models
 {
@@ -9,6 +10,8 @@ namespace ProjectPlanner.Models
         [StringLength(60,MinimumLength =3)]
         public string Name { get; set; }
         public string? Description { get; set; }
+        public IdentityUser User { get; set; }
+
 
     }
 }
